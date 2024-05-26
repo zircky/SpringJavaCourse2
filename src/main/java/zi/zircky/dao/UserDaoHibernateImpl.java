dee892209c3655d56f2517418c84949aa8362bae
ziran <gfgfgfggf02@gmail.com>
2024-05-26 22:07:09 +0300

    Hibernate

--- src/main/java/zi/zircky/dao/UserDaoHibernateImpl.java
+++ src/main/java/zi/zircky/dao/UserDaoHibernateImpl.java
@@ -8,17 +8,22 @@ import zi.zircky.util.Util;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+  Transaction transaction = null;
+
   public UserDaoHibernateImpl() {
   }
 
   @Override
   public void createUsersTable() {
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       String sql = "CREATE TABLE IF NOT EXISTS users (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), lastName VARCHAR(50), age TINYINT)";
       session.createNativeQuery(sql).executeUpdate();
       transaction.commit();
     } catch (Exception e) {
+      if (transaction!= null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
   }
@@ -26,11 +31,14 @@ public class UserDaoHibernateImpl implements UserDao {
   @Override
   public void dropUsersTable() {
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       String sql = "DROP TABLE IF EXISTS users";
       session.createNativeQuery(sql).executeUpdate();
       transaction.commit();
     } catch (Exception e) {
+      if (transaction!= null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
   }
@@ -38,11 +46,14 @@ public class UserDaoHibernateImpl implements UserDao {
   @Override
   public void saveUser(String name, String lastName, byte age) {
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       User user = new User(name, lastName, age);
       session.save(user);
       transaction.commit();
     } catch (Exception e) {
+      if (transaction!= null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
   }
@@ -50,13 +61,16 @@ public class UserDaoHibernateImpl implements UserDao {
   @Override
   public void removeUserById(long id) {
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       User user = session.get(User.class, id);
       if (user != null) {
         session.delete(user);
       }
       transaction.commit();
     } catch (Exception e) {
+      if (transaction != null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
   }
@@ -65,10 +79,13 @@ public class UserDaoHibernateImpl implements UserDao {
   public List<User> getAllUsers() {
     List<User> users = null;
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       users = session.createQuery("from User", User.class).list();
       transaction.commit();
     } catch (Exception e) {
+      if (transaction!= null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
     return users;
@@ -77,11 +94,14 @@ public class UserDaoHibernateImpl implements UserDao {
   @Override
   public void cleanUsersTable() {
     try (Session session = Util.getSessionFactory().openSession()) {
-      Transaction transaction = session.beginTransaction();
+      transaction = session.beginTransaction();
       String sql = "DELETE FROM users";
       session.createNativeQuery(sql).executeUpdate();
       transaction.commit();
     } catch (Exception e) {
+      if (transaction != null) {
+        transaction.rollback();
+      }
       e.printStackTrace();
     }
   }
